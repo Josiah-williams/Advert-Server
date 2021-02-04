78fa786ce13cda9df74a238f53be3cc96f233e2d
josiah <josiahdamiwilliams@gmail.com>
2021-02-04 14:06:21 +0100

    latest update

--- server/knexfile.js
+++ server/knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './database/development.db3'
+      filename: '/server/database/development.db3'
     },
     useNullAsDefault: true,
     pool: {
