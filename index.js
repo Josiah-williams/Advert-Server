73545f057de77050620440e8f9c744b2aafdda3b
josiah <josiahdamiwilliams@gmail.com>
2021-02-10 21:59:30 +0100

    deleted unused components including existing database

--- index.js
+++
@@ -1,6 +0,0 @@
-const server = require('./server/server');
-
-const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`);
-});
