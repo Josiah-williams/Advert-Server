5f7048597735b64c91d8aca26a39a886d1c11182
josiah <josiahdamiwilliams@gmail.com>
2021-02-27 21:47:29 +0100

    added adverts router and configured frontend url

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
-
+const dotenv = require('dotenv');
+const advertRouter = require('./adverts/advertRouter')
 const authRouter = require('./auth/auth-router');
 
 const userRouter = require('./users/userRouter');
 
-
+dotenv.config();
 const server = express();
+
+server.use(
+  cors({
+    origin: [
+      `${process.env.FRONT_URL}`,
+      'http://localhost:3001',
+      'http://localhost:3000',
+      '*',
+    ],
+    credentials: true,
+  })
+);
 
 server.use(helmet());
 server.use(cors());
@@ -15,6 +28,8 @@ server.use(express.json());
 
 server.use('/api/auth', authRouter)
 server.use('/api/user', userRouter)
+server.use('/api/adverts', advertRouter)
+
 
 server.get('/', (req, res) => {
   res.status(200).json('API is working')
