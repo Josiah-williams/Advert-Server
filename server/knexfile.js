bf647c8ca41d1b2ad31bb7abfe2b49b4dba47bac
josiah <josiahdamiwilliams@gmail.com>
2021-05-12 16:06:06 +0100

    configured sqlite to pg

--- server/knexfile.js
+++ server/knexfile.js
@@ -2,18 +2,17 @@
 
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './database/UserAdvert.db3'
+      filename: './database',
+    migrations: {
+      directory: './database/migrations',
     },
     useNullAsDefault: true,
     pool: {
       afterCreate: (conn, done) => {
         conn.run('PRAGMA foreign_keys = ON', done)
       },
-    },
-    migrations: {
-      directory: './database/migrations',
     },
     seeds: {
       directory: './database/seeds',
@@ -34,18 +33,5 @@ module.exports = {
       directory: './database/seeds',
     },
   },
-  
-  testing: { 
-    client: 'sqlite3',
-    connection: { 
-      filename: './database/test.db3',
-    },
-    useNullAsDefault: true, 
-    migrations: { 
-      directory: './database/migrations',
-    },
-    seeds: { 
-      directory: './database/seeds'
-    }
-  }
+},
 };
